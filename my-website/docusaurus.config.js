dfeaabd2af8114c691e465a7c50c390c3f4ec63f
Julien Del Piccolo <julien@del-piccolo.com>
2024-04-24 16:39:13 +0200

    Add github pages config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jdel.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tp2-git',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'jdel', // Usually your GitHub org/user name.
+  projectName: 'tp2-git', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -51,7 +51,6 @@ const config = {
       theme: {
         customCss: './src/css/custom.css',
       },
-      themes: ['@docusaurus/theme-live-codeblock']
     },
   ],
 ],
