5b4e63d2531c703e31a37b047d79d7d22c3d2739
Othniel Hodonou <hodonouothniel@gmail.com>
2024-04-24 14:22:42 +0200

    Modification css

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -35,30 +35,27 @@ const config = {
   },
 
   presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
+  [
+    '@docusaurus/preset-classic',
+    {
+      docs: {
+        sidebarPath: './sidebars.js',
+        editUrl:
+          'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+      },
+      blog: {
+        showReadingTime: true,
+        editUrl:
+          'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+      },
+      theme: {
+        customCss: './src/css/custom.css',
+      },
+      themes: ['@docusaurus/theme-live-codeblock']
+    },
   ],
+],
+
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -139,3 +136,4 @@ const config = {
 };
 
 export default config;
+
